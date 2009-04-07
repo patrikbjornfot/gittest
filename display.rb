b02d333d12882edaed80d906c4d146b935ed90e2
Patrik Bjornfot <patrik.bjornfot@gmail.com>
2009-04-07 16:31:05 +0200

    added display.rb to display branch

--- display.rb
+++ display.rb
@@ -1,6 +1,7 @@
 class Display < MotorCycle
-  def dispAttr
-    puts 'Color of MotorCycle is ' + @color
-    puts 'Make of MotorCycle is ' + @make
-  end
+def dispAttr
+puts 'Color of MotorCycle is ' + @color
+puts 'Make of MotorCycle is ' + @make
 end
+end
+
